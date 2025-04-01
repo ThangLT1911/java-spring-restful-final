919d543214ee591192856dd4ff2fad5ce994a977
Lai The Thang <laithethang00@gmail.com>
2025-04-01 15:55:08 +0700

    update

--- src/main/java/vn/thanglt/jobhunter/controller/AuthController.java
+++ src/main/java/vn/thanglt/jobhunter/controller/AuthController.java
@@ -12,7 +12,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 import vn.thanglt.jobhunter.domain.User;
-import vn.thanglt.jobhunter.domain.dto.LoginDTO;
+import vn.thanglt.jobhunter.domain.dto.ReqLoginDTO;
 import vn.thanglt.jobhunter.domain.dto.ResLoginDTO;
 import vn.thanglt.jobhunter.service.UserService;
 import vn.thanglt.jobhunter.util.SecurityUtil;
@@ -37,7 +37,7 @@ public class AuthController {
     }
 
     @PostMapping("/auth/login")
-    public ResponseEntity<ResLoginDTO> login(@Valid @RequestBody LoginDTO loginDTO) {
+    public ResponseEntity<ResLoginDTO> login(@Valid @RequestBody ReqLoginDTO loginDTO) {
         // Nạp input gồm username/password vào Security
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
                 loginDTO.getUsername(), loginDTO.getPassword());
